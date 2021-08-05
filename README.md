19b74f444e17c152c0cf7c1accccdd1f3a088742
SanghyunPark01 <pash0302@naver.com>
2021-08-05 11:31:03 +0900

    Test 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# OpenCV tutorial  
+OpenCV를 사용하여 몇가지 Test를 하고 기록하는 repository이다.  
+## Test 1  
+Visual Studio 2019에서 이미지를 띄우고 확인해본다.  
+* Visual Studio 와 OpenCV를 연동하는 방법은 추후에 깃허브 블로그에 올릴 예정이다.  
+<p align="center"><img src="https://user-images.githubusercontent.com/77342519/128274689-944e6cc7-6943-435f-9099-665c73801795.png" width="600
+px"></p> 
+<p align="center">실행결과  
+
+## Test 2
+* 미정
---
+++ Test1/Test1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
+using namespace std;
+int main(void)
+{
+    Mat image;
+    image = imread("image.jpg");
+    if (image.empty())
+    {
+        cout << "could not open or find the image" << endl;
+        return -1;
+    }
+    namedWindow("Original", WINDOW_AUTOSIZE);
+    imshow("Original", image);
+    waitKey(0);
+    
+    return 0;
+}
